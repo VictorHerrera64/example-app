ba187402ea19da5c488e8fd8a868f7b162ede5d9
Victor Herrera Ayos <pasiondelbarza@gmail.com>
2024-01-25 12:50:13 -0500

    init project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DomainModule } from './domain/domain.module';
+import { InfraestructureModule } from './infraestructure/infraestructure.module';
+import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [],
+  imports: [DomainModule, InfraestructureModule, CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
